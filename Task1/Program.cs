3734810ecee6990834a1cefa223147564a673033
Vasiya <nva163@mail.ru>
2022-09-29 16:40:35 +0400

    Первая задача

---
+++ Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите количество чисел в массиве");
+
+int size = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [size];
+
+
+void FullArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+        Console.WriteLine(array[i]);
+    }
+
+}
+
+void PositivNumber(int[] array)
+{
+    int result = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        result++;
+    }
+    System.Console.WriteLine($"всего чисел {array.Length}, {result} из них чётные");
+    
+}
+
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " "); // через пробел
+    }
+    Console.WriteLine();
+}
+
+FullArray(array);
+PositivNumber(array);
+PrintArray(array);
